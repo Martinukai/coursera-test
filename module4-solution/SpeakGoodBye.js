f4833765a547b0042b012f2a3d244250ea4cb87e
“Martin <martcrowl@gmail.com>
2021-02-01 23:54:12 +0000

    Fixed space instead of underscore.

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -22,7 +22,7 @@
 	// byeSpeaker object instead of being a standalone function.
 	// See Lecture 52, part 2
 	byeSpeaker.speak = function(name) {
-	  console.log(speakWord + "_" + name);
+	  console.log(speakWord + " " + name);
 	}
 	window.byeSpeaker = byeSpeaker;
 
--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -22,7 +22,7 @@
 	// helloSpeaker object instead of being a standalone function.
 	// See Lecture 52, part 2
 	helloSpeaker.speak = function(name) {
-	  console.log(speakWord + "_" + name);
+	  console.log(speakWord + " " + name);
 	}
 	window.helloSpeaker = helloSpeaker;
 
